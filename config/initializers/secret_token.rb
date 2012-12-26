3e0b8a068f1760aaa460735dd9f8b3b20e691a8d
Adi <zeekisfreak@gmail.com>
2012-12-26 19:37:22 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chimera::Application.config.secret_token = 'b361f2524b916c932bc8867258d19e4aea0e0969d59415c63465d768183d66d42f4aeb2c92d783ccdc6091e3be18353ceec33677d956560bdfd0bcee6f71fe63'
